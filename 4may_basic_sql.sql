b5539102d4eecea83f15bbcdbc027c0cc4eaf854
Tushar Goyal <tushargoyal19tj@gmail.com>
2024-05-04 10:28:54 +0530

    SQL basics

---
+++ 4may_basic_sql.sql
@@ -0,0 +1,31 @@
+
+
+show databases;
+
+use sakila;
+
+-- this is for comment
+/*
+this is a multi line comment
+*/
+show tables;
+
+-- current database
+select database();
+
+show tables;
+
+describe  actor;
+-- select  [kya chaiye]  from table
+select * from actor;
+
+select first_name, last_name,actor_id from actor;
+
+/*
+- What is database? what are the charactestic of database
+- Type of database
+- What is database management system ( DBMS)
+- Database  vs DBMS
+- File system  vs DBMS
+
+*/
